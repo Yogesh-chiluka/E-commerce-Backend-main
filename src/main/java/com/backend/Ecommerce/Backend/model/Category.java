10af3b24d07157e8aeee7d184b9929a994a24f33
Yogesh Chiluka <yogeshravichiluka@gmail.com>
2024-10-09 14:21:31 +0530

    Springboot with MySQL

---
+++ src/main/java/com/backend/Ecommerce/Backend/model/Category.java
@@ -0,0 +1,32 @@
+package com.backend.Ecommerce.Backend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+}
